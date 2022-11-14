81387844359926f410d58e6e50412bb7c63dc44c
Yaroslav19735 <yarkela1997@gmail.com>
2022-11-14 18:12:37 +0200

    second

--- src/api-service.js
+++ src/api-service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function getData(data, page) {
   const apiData = await axios.get(
-    `https://pixabay.com/api/?key=31321520-a449ef359707c8e5cdb01953a=${data}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+    `https://pixabay.com/api/?key=31321520-a449ef359707c8e5cdb01953a&q=${data}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
   );
   return apiData;
 }
